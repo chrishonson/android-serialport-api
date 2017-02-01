c6908990a6c2089b3c80bdb49250fad1fc5b4541
Trilokesh Rangam <rangam.lokesh@gmail.com>
2017-02-01 15:51:48 +0530

    serial port first sample app

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/mips64/objs-debug/serial_port/SerialPort.o.d
@@ -0,0 +1,5 @@
+C:/Users/c_tranga/workspace/serial/app/build/intermediates/ndkBuild/debug/obj/local/mips64/objs-debug/serial_port/SerialPort.o: \
+  C:/Users/c_tranga/workspace/serial/app/src/main/jni/SerialPort.c \
+  C:/Users/c_tranga/workspace/serial/app/src/main/jni/SerialPort.h
+
+C:/Users/c_tranga/workspace/serial/app/src/main/jni/SerialPort.h:
